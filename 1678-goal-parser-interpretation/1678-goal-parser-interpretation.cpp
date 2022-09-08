94e5b56eb69beeab333ba45bfbcf6e995bc299af
jemish_169 <95186825+jemish-169@users.noreply.github.com>
2022-09-08 16:01:17 +0530

    Time: 3 ms (45.46%), Space: 6.2 MB (14.48%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -7,17 +7,16 @@ class Solution
             for (int i = 0; i < c.size(); i++)
             {
                 if (c[i] == 'G')
-                    str.push_back('G');
+                    str += 'G';
                 else if (c[i] == '(' && c[i + 1] == ')')
                 {
                     i++;
-                    str.push_back('o');
+                    str += 'o';
                 }
                 else
                 {
                     i += 3;
-                    str.push_back('a');
-                    str.push_back('l');
+                    str += "al";
                 }
             }
             return str;

ee077f3012b8b23dabdfc26853dc7acf5d7991da
jemish_169 <jemishkhunt169@gmail.com>
2022-09-26 10:39:45 +0530

    Time: 7 ms (8.25%), Space: 6.2 MB (53.75%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -3,7 +3,8 @@ class Solution
     public:
         string toLowerCase(string s)
         {
-            for (int i = 0; i < s.size(); i++)
+            int x = s.size();
+            for (int i = 0; i < x; i++)
             {
                 if (s[i] >= 65 && s[i] <= 90)
                     s[i] += 32;
